59319d390c98f4def3f58aebf7df66cd7c11da79
olayeni45 <olayeni45@gmail.com>
2021-08-11 11:43:06 +0100

    Keeper part 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright &copy; {year}</p>
+        </footer>
+    )
+}
+
+export default Footer;
