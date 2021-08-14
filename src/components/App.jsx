0aed4bc2c1eb2d0c0573ea34df096739b66ea0ec
olayeni45 <olayeni45@gmail.com>
2021-08-15 00:43:23 +0100

    Mapped note contents

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
+import noteCollection from '../notes';
 
 const App = () => {
 
@@ -9,7 +10,16 @@ const App = () => {
 
         <div>
             <Header />
-            <Note />
+
+            {noteCollection.map(note =>
+            (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            )
+            )}
             <Footer />
         </div>
 
